256166a4352e536a343c3ad359dc308661b80c8c
Dayon Czykailo <dayon.czykailo@hotmail.com>
2022-11-02 20:44:44 -0300

    criado e funcionando login,logout e restrição para paginas que precisa estar logado para acessar

---
+++ src/main/java/br/com/api/everywheresystems/controllers/page/PageErrorController.java
@@ -0,0 +1,16 @@
+package br.com.api.everywheresystems.controllers.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageErrorController {
+    
+    @RequestMapping("/404.html")
+    public String render404(Model model) {
+        return "error/404";
+    }
+   
+}
+
